8c3456b200ac9af4b4878b3de750c1618e2b1934
Preet2099x <preet2099x@gmail.com>
2025-10-06 23:10:40 +0530

    added emailverification to provider schema

--- backend/src/models/Provider.js
+++ backend/src/models/Provider.js
@@ -4,7 +4,15 @@ const providerSchema = new mongoose.Schema({
   name: { type: String, required: true },
 
   email: { type: String, required: true, unique: true, lowercase: true },
+  isEmailVerified: { type: Boolean, default: false },
+  emailVerificationCode: { type: String }, // store hashed OTP
+  emailVerificationExpires: { type: Date },
+
   phone: { type: String, required: true, unique: true },
+  isPhoneVerified: { type: Boolean, default: false },
+  phoneVerificationCode: { type: String },
+  phoneVerificationExpires: { type: Date },
+
   password: { type: String, required: true },
 
   businessName: { type: String },
